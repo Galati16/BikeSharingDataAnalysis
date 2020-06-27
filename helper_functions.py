5d51fc0aa0c4fda378a57b11f83a1f1d0d097b1e
Galati16 <galati2@web.de>
2020-06-27 13:33:27 +0200

    Refine comments

--- helper_functions.py
+++ helper_functions.py
@@ -2,6 +2,7 @@ import pandas as pd
 import time
 import numpy as np
 
+# global variables:
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
